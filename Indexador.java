fa695ec3121d26493bf63ae1b2544abc81c10200
Andrieli Mendes <84353670+andrieli31@users.noreply.github.com>
2026-05-26 13:04:14 -0300

    Continuação indexador

--- Indexador.java
+++ Indexador.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.Scanner;
 
 public class Indexador {
 
@@ -12,12 +13,27 @@ public class Indexador {
 
         for (int i = 0; i < arquivos.length; i++) {
             if (arquivos[i].isDirectory()) {
+                //recursão para caso existam sub pastas no diretório
                 indexarDiretorio(arquivos[i].getAbsolutePath());
             }else{
                 if (arquivos[i].getName().endsWith(".txt")) {
-                    System.out.println(arquivos[i].getAbsolutePath());
+                    processarArquivos(arquivos[i]);
                 }
             }
+        }
+    }
+
+    private void processarArquivos(File arquivo){
+        try {
+            Scanner leitor = new Scanner(arquivo);
+            //enquanto existir palavra, continua na mesma linha
+            while (leitor.hasNext()) {
+                String palavra = leitor.next();
+                System.out.println(palavra);
+            }
+            leitor.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
